6aa9123ba73605b29ecbbf332bae55ab4f60a7ee
Han (Omarchy) <hanpham3230@gmail.com>
2026-03-10 15:58:47 -0700

    add leap key binding

---
+++ lua/plugins/leap.lua
@@ -0,0 +1,9 @@
+return {
+  "andyg/leap.nvim",
+  lazy = true,
+  config = function()
+    vim.keymap.set({ "n", "x", "o" }, "s", "<Plug>(leap-forward)")
+    vim.keymap.set({ "n", "x", "o" }, "S", "<Plug>(leap-backward)")
+    vim.keymap.set("n", "gs", "<Plug>(leap-from-window)")
+  end,
+}
